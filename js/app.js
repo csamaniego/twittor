59332dc618e5fb10fb05c06dcc9fa19b049f69a8
Christian Samaniego <samaniegocd@gmail.com>
2019-02-20 11:32:28 -0400

    Get Estimate information of local browser

--- js/app.js
+++ js/app.js
@@ -15,12 +15,73 @@ if ( navigator.serviceWorker ) {
     });
 }
 
+// checking estimate storage
+if ('storage' in navigator && 'estimate' in navigator.storage) {
+    navigator.storage.estimate()
+        .then(function(estimate){
+
+            console.log(`Using ${estimate.usage} out of ${estimate.quota} bytes.`);
+
+        });
+}
+
+const taskStorageEstimate = function(){
+    return new Promise((resolve, reject) => {
+        if (navigator.storage && navigator.storage.estimate && $ && $('#header-fixed-msg')) {
+        navigator.storage.estimate()
+        .then((estimate) => {
+
+            let mSize = (estimate.quota/(1024*1024)).toFixed(2);
+            let uSize = (estimate.usage/(1024*1024)).toFixed(2);
+            if (estimate.usage < 1024) {
+                uSize = estimate.usage.toFixed(2);
+                uSize = uSize + ' bytes';
+            } else if (estimate.usage < (1024*1024)) {
+                uSize = (estimate.usage/1024).toFixed(2);
+                uSize = uSize + ' kbytes';
+            }
+            else {
+                uSize = uSize + ' mbytes';
+            }
+            let msg = `Using ${uSize} out of ${mSize} Mbytes.`;
+            $('#header-fixed-msg').empty().append(msg);
+            console.log(msg);
+            resolve('success');
+
+        })
+        .catch((e) => {
+            reject('failure');
+        });
+        } else {
+            resolve('success');
+        }
+    });
+};
+
+const taskResolution = (period) => {
+    return new Promise((resolve, reject) => {
+      const interval = setInterval(() => {
+        taskStorageEstimate()
+          .then((data) => {
+            if (data === 'failure') {
+                clearInterval(interval);
+                reject(Error('fail'));
+            } else if (data === 'success') {
+                resolve('complete')
+            }
+         });
+      }, period);
+    });
+  };
+
+  taskResolution(10000);
+
 // Referencias de jQuery
 var titulo      = $('#titulo');
 var nuevoBtn    = $('#nuevo-btn');
 var salirBtn    = $('#salir-btn');
 var cancelarBtn = $('#cancel-btn');
-var postBtn     = $('#post-btn');
+var postBtn     = $('.post-btn');
 var avatarSel   = $('#seleccion');
 var timeline    = $('#timeline');
 
@@ -70,8 +131,7 @@ function logIn( ingreso ) {
         timeline.addClass('oculto');
         avatarSel.removeClass('oculto');
 
-        titulo.text('Seleccione Personaje');
-    
+        titulo.empty().append('<i class="fa fa-user"></i>&nbsp;Seleccione Personaje<p id="header-fixed-msg" class="notification-msg"></p>');
     }
 }
 
--- js/sw-utils.js
+++ js/sw-utils.js
@@ -2,9 +2,6 @@
 function updateDynamicCache(dynamic_cache, req, res) {
     if (res.ok) {
         return caches.open(dynamic_cache).then(cache => {
-            // let newResToCache = res.clone();
-            // cache.put(req, newResToCache);
-            // return newResToCache;
             cache.put(req, res.clone());
             return res.clone();
         });
